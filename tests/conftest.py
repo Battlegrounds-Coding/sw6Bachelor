3b1be8f7288d3ddfdcdc7668f5f6966d3f69bc39
DenFlyvendeGed <simont1998@gmail.com>
2024-03-11 11:13:07 +0100

    format

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ In VSCode, Code Coverage is recorded in config.xml. Delete this file to reset re
 
 from __future__ import annotations
 
-#from typing import List
+# from typing import List
 
 import pytest
 from _pytest.nodes import Item
