805cd3956cfde757c09dfc43402c62027d2c5297
DenFlyvendeGed <simont1998@gmail.com>
2024-03-12 11:28:20 +0100

    update

--- src/python_package/__init__.py
+++ src/python_package/__init__.py
@@ -1,7 +1,3 @@
-#   -------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   -------------------------------------------------------------
 """Python Package Template"""
 from __future__ import annotations
 
