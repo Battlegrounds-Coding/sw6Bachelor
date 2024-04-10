803b52cdc5bef06a71e55450ce69172341b0170b
DenFlyvendeGed <simont1998@gmail.com>
2024-04-10 10:08:32 +0200

    working seperated command communication

--- src/python_package/serial/__init__.py
+++ src/python_package/serial/__init__.py
@@ -3,15 +3,20 @@ import serial
 import time 
 
 BAUDRATE =  9600
-COM = "COM4" #<-----
+COM = "COM3" #<-----
 
 arduino = serial.Serial(port=COM, baudrate=BAUDRATE, timeout=10) 
 def write_read(x): 
-	arduino.write(bytes(x, 'utf-8'))
-	time.sleep(0.05)
-	data = arduino.readline() 
-	return data 
+	arduino.write(bytes(x + '\r', 'utf-8'))
+	arduino.write(bytes(" test 2" + '\r', 'utf-8'))
+
+	# arduino.write(x)
+	time.sleep(0.3)
+	# data = arduino.readline() 
+	while(arduino.in_waiting) :
+		print(arduino.read_until(b'\r')) #read all lines
+	return  
 while True: 
-	num = input("Enter a number: ") # Taking input from user 
+	num = input("Enter a number: ") # Taking input from user a
 	value = write_read(num) 
-	print(value) # printing the value 
+	# print(value) # printing the value 
