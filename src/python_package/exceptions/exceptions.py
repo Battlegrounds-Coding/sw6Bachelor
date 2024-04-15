b03126b524af9259e30c8fd6b56888ec16395d0f
DenFlyvendeGed <simont1998@gmail.com>
2024-04-15 10:06:30 +0200

    format

--- src/python_package/exceptions/exceptions.py
+++ src/python_package/exceptions/exceptions.py
@@ -1,6 +1,9 @@
 """Custom exception class for the project"""
+
 from enum import Enum
 
-class Error(Exception,Enum):
+
+class Error(Exception, Enum):
     """Exeption class with enums for specifing error"""
+
     no_response = 1
