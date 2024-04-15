486751482670eb47f50123c9cfb474370bd7fa2d
DenFlyvendeGed <simont1998@gmail.com>
2024-04-15 09:38:07 +0200

    needs custom exceptions

--- src/python_package/serial/__init__.py
+++ src/python_package/serial/__init__.py
@@ -1,22 +1,79 @@
-# Importing Libraries 
-import serial 
-import time 
+"""Contains class for serial communicating over ttf"""
+# Importing Libraries
+import time
+import serial
 
-BAUDRATE =  9600
-COM = "COM3" #<-----
+BAUDRATE = 9600
+COM = "COM3"  # <-----
 
-arduino = serial.Serial(port=COM, baudrate=BAUDRATE, timeout=10) 
-def write_read(x): 
-	arduino.write(bytes(x + '\r', 'utf-8'))
-	arduino.write(bytes(" test 2" + '\r', 'utf-8'))
 
-	# arduino.write(x)
-	time.sleep(0.3)
-	# data = arduino.readline() 
-	while(arduino.in_waiting) :
-		print(arduino.read_until(b'\r')) #read all lines
-	return  
-while True: 
-	num = input("Enter a number: ") # Taking input from user a
-	value = write_read(num) 
-	# print(value) # printing the value 
+class SerialCom:
+    """Serial comunication class"""
+    arduino = serial.Serial(port=COM, baudrate=BAUDRATE, timeout=10)
+
+    def __init__(self, debug=True) -> None:
+        self.debug = debug
+
+    def write(self, x: str) -> None:
+        """Write a bytes package to the connected device, every command is ends on a cardinal return"""
+        self.arduino.write(bytes(x + "\r", "utf-8"))
+
+    def read(self) -> str:
+        """wait for up to 5 seconds on response from connected device,
+          either raise an exception or return the first response"""
+        timeout = time.time() + 5  # 5 seconds from now
+        while not self.arduino.in_waiting:
+            if time.time() > timeout:
+                raise Exception("Response timed out")
+        string = self.arduino.read_until(b"\r").decode()
+        if self.debug:
+            print(string)
+
+        return string
+
+    def set_pump(self, value: int) -> None:
+        """set pump value for connected device, by sending a value with a prefix 'P', 
+        raise an exeption if incorrect value is given"""
+
+        self.write("P" + str(value))
+        rtn = self.read_all()
+        if rtn.pop(-1) == "Please apply a new value:":
+            print("Invalid value, Out of bounds [0..255]")
+            raise Exception("Invalid pump value, Out of bounds [0..255] given value: " + str(value))
+
+    def read_sensor(self) -> int:
+        """Sends a msg with the string 'S' which tells the connected device to return sensor readings"""
+        self.write("S")
+        return int(self.read_all().pop(-1))
+
+    def read_all(self) -> list[str]:
+        """Wait for up to 5 seconds for response from connected device, 
+        either trhow exception or read all responses into an array"""
+        string_array: list[str] = []
+        timeout = time.time() + 5  # 5 seconds from now
+        while not self.arduino.in_waiting:
+            if time.time() > timeout:
+                raise Exception("Response timed out")
+
+        while self.arduino.in_waiting:
+            string = self.arduino.read_until(b"\r").decode()
+            if self.debug:
+                print(string)  # print all lines
+            string_array.append(string)
+        return string_array
+
+    # def write_read(self, x):
+    #     self.write(x)
+    #     self.write(" test 2")
+
+    #     # arduino.write(x)
+    #     time.sleep(0.3)
+    #     # data = arduino.readline()
+    #     while self.arduino.in_waiting:
+    #         print(self.arduino.read_until(b"\r"))  # read all lines
+    #     return
+
+    # while True:
+    # 	num = input("Enter a number: ") # Taking input from user a
+    # 	value = write_read(num)
+    # 	# print(value) # printing the value
