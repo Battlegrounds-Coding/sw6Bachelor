aa8cd7e429ea8ec139afece4ed613994c290b25a
Kamya <peteraksel@live.dk>
2024-04-22 10:02:07 +0200

    added main fn

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 "THIS IS THE MAIN FILE"
 
-import numpy as np
-from python_package.result import Ok
 
-print(Ok(3).value())
+from python_package.log import PrintLogger
 
 
-MSG = "Roll a dice"
-print(MSG)
+if __name__ == '__main__':
+    logger = PrintLogger()
 
-print(np.random.randint(1, 9))
+
