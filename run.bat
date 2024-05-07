879ec9da726e1528ae6977511ce7da8e52bdc0ff
Kamya <peteraksel@live.dk>
2024-05-07 11:01:08 +0200

    Renamed files in bat

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ call "%source%\.venv\Scripts\activate"
 
 if exist "%dir%\" (
     echo Running tests for project %dir%
-    python "%source%\src\main.py" --rain="%dir%\data\fixed\RainData.csv" --mode=headless --data="%dir%\data\fixed\DepthDataFixed.csv" --data-control="%dir%\data\optimal\DepthDataOptimal.csv" --time=7000
+    python "%source%\src\main.py" --rain="%dir%\data\fixed\RainData.csv" --mode=headless --data="%dir%\data\fixed\DepthData.csv" --data-control="%dir%\data\optimal\DepthData.csv" --time=7000
 ) else (
     echo "%dir%" is not a directory
 )
