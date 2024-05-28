dc930492c1014cb8126d58a569054c0de5daddef
DenFlyvendeGed <95428040+DenFlyvendeGed@users.noreply.github.com>
2024-05-28 10:37:34 +0200

    cleanup (#40)

--- src/python_package/exceptions/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""Custom exception class for the project"""
-
-from enum import Enum
-
-
-class Error(Exception, Enum):
-    """Exeption class with enums for specifing error"""
-
-    NO_RESPONSE = 1
